e62e798d62a344b4f46e5dd0233f5a5e5706a0d8
MajesticalMelon <owenfriend2002@gmail.com>
2023-11-27 11:49:06 -0500

    Remove all domo references

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -48,7 +48,7 @@ const signup = async (req, res) => {
     });
     await newAccount.save();
     req.session.account = Account.toAPI(newAccount);
-    return res.json({ redirect: '/' });
+    return res.json({ redirect: '/Witter' });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
